d0ed0ed9c5b5bd139e51ad803457d76e232821de
Oliver Hader <oliver@typo3.org>
2021-11-01 22:22:36 +0100

    Adjust plugin implementation for https://github.com/vimeo/psalm/pull/6789

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,16 +2,21 @@
 
 namespace Olly\Psalm6788;
 
+use Psalm\Plugin\FileExtensionsInterface;
 use Psalm\Plugin\PluginEntryPointInterface;
+use Psalm\Plugin\PluginFileExtensionsInterface;
 use Psalm\Plugin\RegistrationInterface;
 use SimpleXMLElement;
 
-class Plugin implements PluginEntryPointInterface
+class Plugin implements PluginEntryPointInterface, PluginFileExtensionsInterface
 {
     public function __invoke(RegistrationInterface $registration, ?SimpleXMLElement $config = null): void
     {
-        $registration->addFileTypeScanner('any', AnyScanner::class);
-        $registration->addFileTypeAnalyzer('any', AnyAnalyzer::class);
-        var_dump('registered');
+    }
+
+    public function processFileExtensions(FileExtensionsInterface $fileExtensions, ?SimpleXMLElement $config = null): void
+    {
+        $fileExtensions->addFileTypeScanner('any', AnyScanner::class);
+        $fileExtensions->addFileTypeAnalyzer('any', AnyAnalyzer::class);
     }
 }
